13a510db4f3d58ce5653f64960aade8aebff12c2
gitdeepaks <deepaksankhyan92@outlook.com>
2023-11-14 13:59:45 +0530

    added navbar 🚀

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,11 +2,14 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="item-center flex gap-1">
+      <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
           width={23}
@@ -15,12 +18,12 @@ const Navbar = () => {
         />
 
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden ">
-          NextDev<span className="text-primary-500">OverFlow</span>
+          NextDev<span className="text-primary-500">Flow</span>
         </p>
       </Link>
-      GlobalSearch
+      <GlobalSearch />
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
@@ -34,7 +37,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        MobileNavBar
+        <MobileNav />
       </div>
     </nav>
   );
