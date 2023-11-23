c7be7b21e7a58cb3f3a796a265c9360750f4e35f
gitdeepaks <deepaksankhyan92@outlook.com>
2023-11-23 21:02:10 +0530

    added tags components 🚀

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,24 +1,39 @@
+"use server";
+
 import User from "@/database/user.model";
 import { connectToDataBase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import console from "console";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDataBase();
 
     const { userId } = params;
+
     const user = await User.findById(userId);
 
-    if (!user) {
-      throw new Error("User not found");
-    }
+    if (!user) throw new Error("User not found");
 
-    //   find interactionsfor the user and group by tags...
+    // Find interactions for the user and group by tags...
+    // Interaction...
 
     return [
-      { _id: "1", name: "tag1" },
-      { _id: " 2", name: "tag2" },
+      { _id: "1", name: "tag" },
+      { _id: "2", name: "tag2" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDataBase();
+    const tags = await Tag.find({});
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
