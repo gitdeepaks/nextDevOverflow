7b93a662e00de0e47ac32f727dc6c239bcbcdda8
gitdeepaks <deepaksankhyan92@outlook.com>
2023-11-23 13:26:43 +0530

    added webhook secret  🚀

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -9,7 +9,7 @@ import { ok } from "assert";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   // TODO: Add your Clerk Webhook Secret to .env.local
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
