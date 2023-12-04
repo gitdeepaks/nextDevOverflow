26b77344ab444dc506ccdedee8246df2452307a5
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-04 17:19:15 +0530

    added global search  🚀

--- pagination.js
+++
@@ -1,6 +0,0 @@
-const skipAmount = (page - 1) * pageSize;
-.skip(skipAmount)
-.limit(pageSize)
-const totalQuestions = await Question.countDocuments(query);
-
-const isNext = totalQuestions > skipAmount + questions.length;
